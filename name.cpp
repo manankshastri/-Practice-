ed0d4385e37f74e2036b5310d0b0b3d29ae5e2a7
manankshastri <manank.shastri@gmail.com>
2019-03-21 16:52:08 -0400

    Initial commit

---
+++ name.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"hey, writing std:: is pain..";
+    cout<<"change the program so that I don't have to write it.";
+    
+    return 0;
+}
